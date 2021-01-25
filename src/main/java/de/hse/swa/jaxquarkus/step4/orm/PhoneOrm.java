4e1d2dfa73bcfae9b8244f06d67e0a9cdfd4876e
Voxel07 <voxel@vivaldi.net>
2021-01-26 00:17:41 +0100

    fixed formating

--- src/main/java/de/hse/swa/jaxquarkus/step4/orm/PhoneOrm.java
+++ src/main/java/de/hse/swa/jaxquarkus/step4/orm/PhoneOrm.java
@@ -6,21 +6,19 @@ import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
 import javax.transaction.Transactional;
-
-import de.hse.swa.jaxquarkus.step4.model.Phone;
-import de.hse.swa.jaxquarkus.step4.model.User;
+import de.hse.swa.jaxquarkus.step4.model.*;
 
 @ApplicationScoped
-public class PhoneOrm{
-	
+public class PhoneOrm {
+
 	@Inject
 	EntityManager em;
 
-	public List<Phone> getPhones(){
+	public List<Phone> getPhones() {
 		TypedQuery<Phone> query = em.createQuery("SELECT p FROM Phone p", Phone.class);
-     	return query.getResultList();
+		return query.getResultList();
 	}
-	
+
 	public List<Phone> getPhoneByNumber(String number) {
 		System.out.println("PhoneORM/getPhoneByNumber");
 		TypedQuery<Phone> query = em.createQuery("SELECT p FROM Phone p WHERE p.number =:val", Phone.class);
@@ -28,118 +26,60 @@ public class PhoneOrm{
 		return query.getResultList();
 	}
 
-    public List<Phone> getUserPhones(Long usrId){
+	public List<Phone> getUserPhones(Long usrId) {
 		System.out.println("PhoneORM/getUserPhones");
-    	TypedQuery<Phone> query = em.createQuery("SELECT p FROM Phone p WHERE user_id = :val", Phone.class);
-    	query.setParameter("val", usrId);
-    	return query.getResultList();
-    }
-	
+		TypedQuery<Phone> query = em.createQuery("SELECT p FROM Phone p WHERE user_id = :val", Phone.class);
+		query.setParameter("val", usrId);
+		return query.getResultList();
+	}
+
 	@Transactional
 	public Boolean updatePhone(Phone phone) {
-		if(getPhoneByNumber(phone.getNumber()).isEmpty()){
+		if (getPhoneByNumber(phone.getNumber()).isEmpty()) {
 			em.merge(phone);
 			return true;
-		}
-		else{
+		} else {
 			return false;
 		}
 	}
-	
+
 	@Transactional
-    public Boolean addPhone(Long usrId, Phone p) {
-		
-		if(!getPhoneByNumber(p.getNumber()).isEmpty()) {
+	public Boolean addPhone(Long usrId, Phone p) {
+
+		if (!getPhoneByNumber(p.getNumber()).isEmpty()) {
 			return false;
 		}
-		if((getUserPhones(usrId).size() >= 2)) {
+		if ((getUserPhones(usrId).size() >= 2)) {
 			return false;
 		}
-		
+
 		User usr = em.find(User.class, usrId);
-		
-		
+
 		usr.getPhones().add(p);
-		//hat gefehlt
+		// hat gefehlt
 		p.setUsr(usr);
 		em.persist(p);
 		em.merge(usr);
 		return true;
-		
-		
-		
-		
-//    	Boolean duplicate = false;
-//    	int anzNumber = 0; 
-//    	String error = "Max anz erreicht";
-//    	
-//    	System.out.println("Aus der ORM: "+" ID: "+usrId+" number: "+p.getNumber()+" type: "+p.getType());  	
-//    	//Pr�fen dass nur zwei nummern da sind
-//		TypedQuery<Phone> query =em.createQuery("SELECT p FROM Phone p WHERE user_id = :val OR p.number = :val2", Phone.class);
-//		query.setParameter("val", usrId);
-//		query.setParameter("val2", p.getNumber());
-//		
-//		System.out.println("AnzResultsQuery: "+ query.getResultList().size());
-//		
-//		//Check all Numbers
-//	
-//		for(Phone elem : query.getResultList()) {
-//			System.out.println("AktElement: "+ elem );
-//			//if(elem.getNumber()==number) geht nicht aus Gr�nden 
-//			//Check for duplicate entry and anzNumbers
-//			if(elem.getNumber().equals(p.getNumber())) {
-//				System.out.println("ComparingNumbers: " +elem.getNumber()+" to "+p.getNumber() );
-//				duplicate = true;
-//				error = "Doppelte Nr entdeckt bei User: "+elem.getId();
-//				break;
-//			}
-//		}
-//  
-//		if(query.getResultList().size() <= 1 && duplicate==false) {	
-//			System.out.println("If erreicht mit "+anzNumber+" dub? "+ duplicate);
-//			User usr = new User();
-//			
-//			try {
-//				usr = em.find(User.class, usrId);
-//			} catch (Exception e) {
-//				e.printStackTrace();
-//				System.out.println("Custom Exception UserOrm addPhone Find User: "+ e.toString());
-//				return e.toString();
-//			}
-//			Phone ph = new Phone(p.getNumber(),p.getType());
-//			usr.getPhones().add(ph);
-//			//hat gefehlt
-//			ph.setUsr(usr);
-//			em.persist(usr);
-//			return "Phone added";
-//		}
-//		else {
-//			System.out.println(error);
-//			return error;
-//		}	
-	}
-    
-    @Transactional
-    public Boolean removePhoneFromUser(Phone p) {
-		return	em.createQuery("DELETE FROM Phone WHERE number =: val")/*Ich bin Wichtig !!*/
-		.setParameter("val", p.getNumber())
-		.executeUpdate()==1;
+
 	}
 
 	@Transactional
-    public Boolean removeAllPhonesFromUser(User u) {
+	public Boolean removePhoneFromUser(Phone p) {
+		return em.createQuery("DELETE FROM Phone WHERE number =: val")
+				.setParameter("val", p.getNumber()).executeUpdate() == 1;
+	}
+
+	@Transactional
+	public Boolean removeAllPhonesFromUser(User u) {
 		System.out.println("PhoneOrm/removeAllPhonesFromUser");
-		if(!getUserPhones(u.getId()).isEmpty()){
-			int anz = em.createQuery("DELETE FROM Phone WHERE user_id =: val")/*Ich bin Wichtig !!*/
-			.setParameter("val", u.getId())
-			.executeUpdate();
-			return anz == 1 || anz ==2;
-		}
-		else{
+		if (!getUserPhones(u.getId()).isEmpty()) {
+			int anz = em.createQuery("DELETE FROM Phone WHERE user_id =: val")
+					.setParameter("val", u.getId()).executeUpdate();
+			return anz == 1 || anz == 2;
+		} else {
 			return true;
 		}
-    }
-
+	}
 
 }
-
--- src/test/java/de/hse/swa/jaxquarkus/phone_t.java
+++ src/test/java/de/hse/swa/jaxquarkus/phone_t.java
@@ -4,276 +4,178 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
-
-import de.hse.swa.jaxquarkus.step4.model.Company;
-import de.hse.swa.jaxquarkus.step4.model.Contract;
-import de.hse.swa.jaxquarkus.step4.model.IpNumber;
-import de.hse.swa.jaxquarkus.step4.model.Phone;
-import de.hse.swa.jaxquarkus.step4.model.User;
-import de.hse.swa.jaxquarkus.step4.orm.CompanyOrm;
-import de.hse.swa.jaxquarkus.step4.orm.PhoneOrm;
-
+import de.hse.swa.jaxquarkus.step4.model.*;
+import de.hse.swa.jaxquarkus.step4.orm.*;
 import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.response.Response;
-
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
-
 import java.util.Arrays;
 import java.util.List;
-
 import javax.ws.rs.core.MediaType;
 import javax.inject.Inject;
+
 @QuarkusTest
 @TestMethodOrder(OrderAnnotation.class)
-public class phone_t{
-	private static Phone phoneA = new Phone("Anumber", "Atype");  
+public class phone_t {
+	private static Phone phoneA = new Phone("Anumber", "Atype");
 	private static Phone phoneB = new Phone("Bnumber", "Btype");
 	private static Phone phoneC = new Phone("Cnumber", "Ctype");
-    private static Company companyA = new Company("Aname", "Adepartment", "Astreet", 12345, "Astate", "Acountry");	
+	private static Company companyA = new Company("Aname", "Adepartment", "Astreet", 12345, "Astate", "Acountry");
 	private static User usrA = new User("Aemail", "Ausername", "Apassword", "Afirst", "Alast", true);
 
 	@Inject
-    CompanyOrm companyOrm;
-	
+	CompanyOrm companyOrm;
+
 	@Test
 	@Order(1)
-	public void addPhoneToUser(){
-		
-		given()
-		.contentType(MediaType.APPLICATION_JSON)
-		.body(companyA)
-			.when()
-			.put("/company")
-			.then().statusCode(200).body(is("Company hinzugefügt"));
-		
-		Response response =
-				given()
-				.contentType(MediaType.APPLICATION_JSON)
-				.when()
-				.get("/company");
-				
-				response
-				.then()
-				.statusCode(200);
-				
-	 List<Company> companys = Arrays.asList(response.getBody().as(Company[].class));
+	public void addPhoneToUser() {
+
+		given().contentType(MediaType.APPLICATION_JSON).body(companyA).when().put("/company").then().statusCode(200)
+				.body(is("Company hinzugefügt"));
+
+		Response response = given().contentType(MediaType.APPLICATION_JSON).when().get("/company");
+
+		response.then().statusCode(200);
+
+		List<Company> companys = Arrays.asList(response.getBody().as(Company[].class));
 		companyA.setId(companys.get(0).getId());
-		
-		given()
-		 .pathParam("companyId", companyA.getId())
-		 .contentType(MediaType.APPLICATION_JSON)
-		 .body(usrA)
-		 .when()
-		 .put("/user/{companyId}")	
-		 .then().statusCode(200).body(is(""+companyA.getId()));
-		
-		response =
-     	        given()     	    
-     	        .contentType(MediaType.APPLICATION_JSON)
-     	        .when()
-     	        .get("/user");		      	    	
- 	        	response
-     	        .then()
-     	        .statusCode(200);
-		      	        
-	      		List<User> usrs = Arrays.asList(response.getBody().as(User[].class));
-	      		usrA.setId(usrs.get(0).getId());
-		
-		 given()
-		 	.pathParam("id", companyA.getId())
-	        .contentType(MediaType.APPLICATION_JSON)
-	        .body(phoneA)
-	        .when()
-	        .put("/phone/{id}")	
-	        .then()
-	        .statusCode(200).body(is("true"));
-		 
-		 response = 
-				 	given()
-					.queryParam("usrId", usrA.getId())
-					.contentType(MediaType.APPLICATION_JSON)
-					.when()
-					.get("/phone");	
-					
-		      		response.then().statusCode(200);
-					List<Phone> phones = Arrays.asList(response.getBody().as(Phone[].class));
-					phoneA.setId(phones.get(0).getId());
-		}
-	
+
+		given().pathParam("companyId", companyA.getId()).contentType(MediaType.APPLICATION_JSON).body(usrA).when()
+				.put("/user/{companyId}").then().statusCode(200).body(is("" + companyA.getId()));
+
+		response = given().contentType(MediaType.APPLICATION_JSON).when().get("/user");
+		response.then().statusCode(200);
+
+		List<User> usrs = Arrays.asList(response.getBody().as(User[].class));
+		usrA.setId(usrs.get(0).getId());
+
+		given().pathParam("id", companyA.getId()).contentType(MediaType.APPLICATION_JSON).body(phoneA).when()
+				.put("/phone/{id}").then().statusCode(200).body(is("true"));
+
+		response = given().queryParam("usrId", usrA.getId()).contentType(MediaType.APPLICATION_JSON).when()
+				.get("/phone");
+
+		response.then().statusCode(200);
+		List<Phone> phones = Arrays.asList(response.getBody().as(Phone[].class));
+		phoneA.setId(phones.get(0).getId());
+	}
+
 	@Test
 	@Order(2)
-	public void addPhoneToUserDuplicate(){
-		
-		 given()
-		    .pathParam("id", companyA.getId())
-	        .contentType(MediaType.APPLICATION_JSON)
-	        .body(phoneA)
-	        .when()
-	        .put("/phone/{id}")	
-	        .then()
-	        .statusCode(200).body(is("false"));
+	public void addPhoneToUserDuplicate() {
 
-		Response response = 
-				 	given()
-					.queryParam("usrId", usrA.getId())
-					.contentType(MediaType.APPLICATION_JSON)
-					.when()
-					.get("/phone");	
-					
-		      		response.then().statusCode(200);
-					List<Phone> phones = Arrays.asList(response.getBody().as(Phone[].class));
-					phoneA.setId(phones.get(0).getId());
-		}
-	
+		given().pathParam("id", companyA.getId()).contentType(MediaType.APPLICATION_JSON).body(phoneA).when()
+				.put("/phone/{id}").then().statusCode(200).body(is("false"));
+
+		Response response = given().queryParam("usrId", usrA.getId()).contentType(MediaType.APPLICATION_JSON).when()
+				.get("/phone");
+
+		response.then().statusCode(200);
+		List<Phone> phones = Arrays.asList(response.getBody().as(Phone[].class));
+		phoneA.setId(phones.get(0).getId());
+	}
+
 	@Test
 	@Order(3)
-	public void addPhoneToUser2(){
+	public void addPhoneToUser2() {
 
-		 given()
-		 .pathParam("id", companyA.getId())
-	        .contentType(MediaType.APPLICATION_JSON)
-	        .body(phoneB)
-	        .when()
-	        .put("/phone/{id}")	
-	        .then()
-	        .statusCode(200).body(is("true"));
-		 
-		 Response response = 
-				 	given()
-					.queryParam("usrId", usrA.getId())
-					.contentType(MediaType.APPLICATION_JSON)
-					.when()
-					.get("/phone");	
-					
-		      		response.then().statusCode(200);
-					List<Phone> phones = Arrays.asList(response.getBody().as(Phone[].class));
-					phoneB.setId(phones.get(0).getId());
+		given().pathParam("id", companyA.getId()).contentType(MediaType.APPLICATION_JSON).body(phoneB).when()
+				.put("/phone/{id}").then().statusCode(200).body(is("true"));
 
-		}
+		Response response = given().queryParam("usrId", usrA.getId()).contentType(MediaType.APPLICATION_JSON).when()
+				.get("/phone");
+
+		response.then().statusCode(200);
+		List<Phone> phones = Arrays.asList(response.getBody().as(Phone[].class));
+		phoneB.setId(phones.get(0).getId());
+	}
+
 	@Test
 	@Order(4)
-	public void addPhoneToUserToMannyNumbers(){
-		
-		 given()
-		    .pathParam("id", companyA.getId())
-	        .contentType(MediaType.APPLICATION_JSON)
-	        .body(phoneC)
-	        .when()
-	        .put("/phone/{id}")	
-	        .then()
-	        .statusCode(200).body(is("false"));
+	public void addPhoneToUserToMannyNumbers() {
 
-		}
+		given().pathParam("id", companyA.getId()).contentType(MediaType.APPLICATION_JSON).body(phoneC).when()
+				.put("/phone/{id}").then().statusCode(200).body(is("false"));
+	}
 
 	@Test
 	@Order(5)
 	public void getUserPhones() {
-		Response res = given()
-		.queryParam("usr_id", usrA.getId())
-		.contentType(MediaType.APPLICATION_JSON)
-		.when()
-		.get("/phone");	
-		
+		Response res = given().queryParam("usr_id", usrA.getId()).contentType(MediaType.APPLICATION_JSON).when()
+				.get("/phone");
+
 		res.then().statusCode(200);
 		List<Phone> phones = Arrays.asList(res.getBody().as(Phone[].class));
-		Assertions.assertEquals( phoneA.getNumber(), phones.get(0).getNumber());
-		Assertions.assertEquals( phoneB.getNumber(), phones.get(1).getNumber());
-		Assertions.assertEquals( 2, phones.size());
+		Assertions.assertEquals(phoneA.getNumber(), phones.get(0).getNumber());
+		Assertions.assertEquals(phoneB.getNumber(), phones.get(1).getNumber());
+		Assertions.assertEquals(2, phones.size());
 	}
 
 	@Test
 	@Order(6)
 	public void removePhoneFromUser() {
-		 //phoneB.setId(1l);
-		 
-		 given()
-	        .contentType(MediaType.APPLICATION_JSON)
-	        .body(phoneA)
-	        .when()
-	        .delete("/phone")	
-	        .then()
-	        .statusCode(200).body(is("true"));
+		given().contentType(MediaType.APPLICATION_JSON).body(phoneA).when().delete("/phone").then().statusCode(200)
+				.body(is("true"));
 	}
 
 	@Test
 	@Order(7)
 	public void getUserPhones2() {
-		Response res = given()
-		.queryParam("number", phoneB.getNumber())
-        .contentType(MediaType.APPLICATION_JSON)
-        .when()
-		.get("/phone");	
-       
+		Response res = given().queryParam("number", phoneB.getNumber()).contentType(MediaType.APPLICATION_JSON).when()
+				.get("/phone");
+
 		res.then().statusCode(200);
 		List<Phone> phones = Arrays.asList(res.getBody().as(Phone[].class));
-		Assertions.assertEquals( phoneB.getNumber(), phones.get(0).getNumber());
-		Assertions.assertEquals( 1, phones.size());
+		Assertions.assertEquals(phoneB.getNumber(), phones.get(0).getNumber());
+		Assertions.assertEquals(1, phones.size());
 	}
 
 	@Test
 	@Order(8)
-	public void updatePhone(){
+	public void updatePhone() {
 		phoneB.setType("ich bin geaendert");
 		phoneB.setNumber("9876543");
-		//phoneB.setId(2l);
-		given()
-		.contentType(MediaType.APPLICATION_JSON)
-		.body(phoneB)
-        .when()
-		.post("/phone")	
-		.then()
-		.statusCode(200).body(is("true"));	
-		
-		Response res = given()
-		.queryParam("number", phoneB.getNumber())
-        .contentType(MediaType.APPLICATION_JSON)
-        .when()
-		.get("/phone");	
-       
+
+		given().contentType(MediaType.APPLICATION_JSON).body(phoneB).when().post("/phone").then().statusCode(200)
+				.body(is("true"));
+
+		Response res = given().queryParam("number", phoneB.getNumber()).contentType(MediaType.APPLICATION_JSON).when()
+				.get("/phone");
+
 		res.then().statusCode(200);
 		List<Phone> phones = Arrays.asList(res.getBody().as(Phone[].class));
-		Assertions.assertEquals( phoneB.getNumber(), phones.get(0).getNumber());
-		Assertions.assertEquals( phoneB.getType(), phones.get(0).getType());
-		Assertions.assertEquals( 1, phones.size());
-	
-	}
-	@Test
-	@Order(9)
-	public void updatePhonenoachmal(){
-		phoneB.setType("ich bin geaendert nochmal nochmal");
-		phoneB.setNumber("Bnumber2");
-		//phoneB.setId(companyA.getId());
-		given()
-		.contentType(MediaType.APPLICATION_JSON)
-		.body(phoneB)
-        .when()
-		.post("/phone")
-		.then()
-		.statusCode(200).body(is("true"));	
+		Assertions.assertEquals(phoneB.getNumber(), phones.get(0).getNumber());
+		Assertions.assertEquals(phoneB.getType(), phones.get(0).getType());
+		Assertions.assertEquals(1, phones.size());
+
 	}
 
-	// @Test
+	@Test
+	@Order(9)
+	public void updatePhonenoachmal() {
+		phoneB.setType("ich bin geaendert nochmal nochmal");
+		phoneB.setNumber("Bnumber2");
+
+		given().contentType(MediaType.APPLICATION_JSON).body(phoneB).when().post("/phone").then().statusCode(200)
+				.body(is("true"));
+	}
+
+	@Test
 	@Order(10)
 	public void removeAllPhoneFromUser() {
-	User usrC = new User("Cemail", "Cusername", "Cpassword", "Cfirst", "Clast", true); 
-		//phoneB.setId(1l);
+
+		User usrC = new User("Cemail", "Cusername", "Cpassword", "Cfirst", "Clast", true);
 		usrC.setId(companyA.getId());
-	//	phoneOrm.removeAllPhonesFromUser(usrC);
-		 
-		 given()
-	        .contentType(MediaType.APPLICATION_JSON)
-	        .body(usrC)
-	        .when()
-	        .delete("/phone/all")	
-	        .then()
-	        .statusCode(200).body(is("true"));
+
+		given().contentType(MediaType.APPLICATION_JSON).body(usrC).when().delete("/phone/all").then().statusCode(200)
+				.body(is("true"));
 	}
-	// @Test
+
+	@Test
 	@Order(11)
-	public void deleteall(){
-		//Delete all
-			companyOrm.deleteall();			
+	public void deleteall() {
+		companyOrm.deleteall();
 	}
 }
